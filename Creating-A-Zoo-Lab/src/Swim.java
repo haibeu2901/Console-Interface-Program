36a8e6fb412d9e52664bde021e4221fb23143ded
Duc Hai Doan <111358742+haibeu2901@users.noreply.github.com>
2025-04-24 09:37:08 +0700

    Add files via upload

---
+++ Creating-A-Zoo-Lab/src/Swim.java
@@ -0,0 +1,8 @@
+public interface Swim {
+    /** TODO 6: create a new interface named "Swim"
+     * and declaring a method inside it
+     * named "swimming" with the return type
+     * "void"
+     **/
+    void swimming();
+}
